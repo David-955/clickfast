325f43ecbd989ebf8ed217513041b8c3ae49d701
David-955 <caotien.ngo@lepoles.org>
2025-01-08 16:11:02 +0100

    ajout bouton reset

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ let count = 0;
 let timerStarted = false;
 let timeLeft = 5;
 
+function handleGameButton() {
 document
   .getElementById("button-clicker")
   .addEventListener("click", () => {
     if (!timerStarted) {
+      document.getElementById("button-reset").disabled = true;
       timerStarted = true;
       count = 0;
       timeLeft = 5;
@@ -21,6 +23,7 @@ document
         if (timeLeft <= 0) {
           clearInterval(timerInterval);
           document.getElementById("button-clicker").disabled = true;
+          document.getElementById("button-reset").disabled = false;
           document.getElementById("score").innerHTML = `Ton score est : ${count}`;
         }
       }, 1000);
@@ -29,7 +32,22 @@ document
     count++;
     document.getElementById("counter").innerHTML = count;
 
-  });
+  })
+};
 
+  function handleResetButton() {
+    const resetButton = document.getElementById("button-reset");
   
+    resetButton.addEventListener("click", () => {
+      // canPlay = true;
+      timerStarted = false;
+      count = 0;
+      document.getElementById("score").innerHTML = count;
+      document.getElementById("counter").innerHTML = `${count}`;
+      document.getElementById("button-clicker").disabled = false;
+    });
+  }
+  
+  handleGameButton();
+  handleResetButton();
 
